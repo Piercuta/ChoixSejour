ae4c098b117a74446adba57e3e2a1c6e87ba262a
Piercuta <pierre.courteille@gmail.com>
2022-03-25 12:00:24 +0100

    add view models

---
+++ ChoixSejour/ViewModels/SejourVoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixSejour.ViewModels
+{
+    public class SejourVoteViewModel
+    {
+        public List<SejourCheckBoxViewModel> ListeDesSejours { get; set; }
+    }
+}
