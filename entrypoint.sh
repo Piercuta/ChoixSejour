657c90c6d33bdadba1f55f9a74e4978d9916db6f
Piercuta <pierre.courteille@gmail.com>
2022-05-04 15:39:08 +0200

    update docker files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-set -e
-run_cmd="dotnet ChoixSejour.dll"
+CS="server=$DB_HOST;uid=root;pwd=$DB_PASSWORD;database=ChoixSejour;"
 
-CS="server=$DBHOST;uid=root;pwd=Coucou123!;database=ChoixSejour;"
-
+echo "Connection string : "
 echo $CS
 
+echo "Replace Connection string in appsettings.json"
 find . -type f -name "appsettings.json" -print0 | xargs -0 sed -i -e "s|{cs}|$CS|g"
 
+echo "sleep 30 seconds..."
 sleep 30
 
-exec $run_cmd
+echo "Choix Sejour is running..."
+dotnet ChoixSejour.dll
