61e6717a21420c6b2b136b2c679a4970d7df4f76
piercuta <pierre.courteille@gmail.com>
2022-05-04 17:03:39 +0000

    fix files problem

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 # syntax=docker/dockerfile:1
 FROM mcr.microsoft.com/dotnet/sdk:3.1
 WORKDIR /app
-RUN ["dotnet", "publish", "o", "website"]
-COPY website /app
-COPY entrypoint.sh /app
+COPY . . 
 EXPOSE 5000/tcp
 EXPOSE 5001/tcp
 RUN chmod +x ./entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Publish project..."
+
+curl https://api.nuget.org/v3/index.json -k
+
+dotnet publish -o website
+
 CS="server=$DB_HOST;uid=root;pwd=$DB_PASSWORD;database=ChoixSejour;"
 
 echo "Connection string : "
@@ -12,4 +18,6 @@ echo "sleep 30 seconds..."
 sleep 30
 
 echo "Choix Sejour is running..."
+cd website/
+cat appsettings.json
 dotnet ChoixSejour.dll
